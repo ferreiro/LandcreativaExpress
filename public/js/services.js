a913edc532dd05ae97a502684ea399caa46838d2
Jorge Ferreiro <me@jgferreiro.com>
2015-08-09 19:35:56 +0200

    Awesome feature. Fixing buy tabs on services

--- public/js/services.js
+++ public/js/services.js
@@ -1,32 +1,80 @@
+// Stuck my element
 
-//----------------------------
-//-- SERVICIOS
-//----------------------------
-// Esto tiene que estar al final del todo...
-
-var elementPosition = $('.productBox-button').offset();
 
 $(window).scroll(function(){
-	total = elementPosition.top - $(window).scrollTop();
-	
-	// console.log('Button top ' + elementPosition.top);
-	// console.log('Scroll top ' + $(window).scrollTop());
-	// console.log(total);
+	var boxes = $('.services-content-box');
+	var currentBox, currentFixedPannel;
+	var e = $(this); // window
+	// console.log(window.scrollY);
 
-    if(total <= 0){
-    	// Display element
+	// Get the current box that is being shown
+	for (i = 0; i < boxes.length; i++) {
+		element = $('.services-content-box:eq('+ i +')');
+		if (element.hasClass('services-content-box-showbox')) {
+			currentBox = element;
+		}
+	}
 
-    	$('.services-fixedbuypannel').fadeIn({
-    		duration: 300,
-    		easing: 'easeInOutCirc'
-    	});
-    	console.log("entra"); 
+	// Saber si el currentbox ha tocado la cabecera de la web.
 
-    } else {
-    	$('.services-fixedbuypannel').hide(300);
-    	console.log("sal"); 
-    }    
+	var elementOffsetTop = currentBox.offset().top;
+	var header = 80;
+	var pannel = currentBox.find('.services-buypannel');
+	pannel.removeClass('endpin');
+
+	if ($(window).scrollTop() >= elementOffsetTop - header) {
+		// The current box has reached screen top
+		var insideTotalScroll = (currentBox.height() + currentBox.offset().top + pannel.height()) - $(window).scrollTop() - $(window).height();
+
+		pannel.fadeIn(100);
+		pannel.addClass('stuck');
+
+		if (insideTotalScroll > 0) {
+			// Fijar el panel
+			pannel.addClass('stuck');
+		}
+		else {
+			// Poner el panel como absolute
+			pannel.removeClass('stuck');
+			pannel.addClass('endpin');
+		}
+	} 
+
 });
+
+
+
+
+
+// //----------------------------
+// //-- SERVICIOS
+// //----------------------------
+// // Esto tiene que estar al final del todo...
+
+// var elementPosition = $('.productBox-button').offset();
+
+// $(window).scroll(function(){
+// 	total = elementPosition.top - $(window).scrollTop();
+	
+// 	// console.log('Button top ' + elementPosition.top);
+// 	// console.log('Scroll top ' + $(window).scrollTop());
+// 	// console.log(total);
+
+//     if(total <= 0){
+//     	// Display element
+
+//     	$('.services-buypannel').fadeIn({
+//     		duration: 0,
+//     		easing: 'easeInOutCirc'
+//     	});
+//     	console.log("entra"); 
+
+//     } else {
+//     	$('.services-buypannel').hide(0);
+//     	console.log("sal"); 
+//     }    
+//     $('.services-buypannel').show(0);
+// });
 
 
 //----------------------------
@@ -44,6 +92,7 @@ var changeTab = function changeTab(newOption) {
 	}
 	else {
 		tabElements.attr('class', 'services-tab-item'); // remove all classes except the base one...
+		tabContent.removeClass('services-content-box-showbox');
 		tabContent.hide(0);			// Hide the tab content
 		selectsMenu(newOption);		// Add selected class
 		displayContent(newOption);	// Add selected class
@@ -67,6 +116,7 @@ function displayContent(number) {
 	
 	var content = $('.services-content-box:eq('+number+')');	// Seletcs the content
 	content.fadeIn("slow");					// Show content
+	content.addClass('services-content-box-showbox');
 }
 
 // Implementación concreta para este proyecto.
